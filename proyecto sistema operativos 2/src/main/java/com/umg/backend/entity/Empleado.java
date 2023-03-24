4a14c136581460d6ca01e334b7021e6498a1c1ef
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-03-23 22:51:43 -0600

    creacion de servicio

---
+++ proyecto sistema operativos 2/src/main/java/com/umg/backend/entity/Empleado.java
@@ -0,0 +1,69 @@
+package com.umg.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name="empleado")
+public class Empleado {
+
+    @Id
+    @Column(name = "idempleado",nullable = false)
+    private Integer idEmpleado;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "fecha_nacimiento")
+    private Date fechaNacimiento;
+
+    @Column(name = "dpi")
+    private Integer dpi;
+
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Integer getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(Integer dpi) {
+        this.dpi = dpi;
+    }
+}
---
+++ proyecto sistema operativos 2/src/main/java/com/umg/backend/service/EmpleadoServices.java
@@ -0,0 +1,25 @@
+package com.umg.backend.service;
+
+
+import com.umg.backend.entity.Empleado;
+import com.umg.backend.repository.empleadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("empleado")
+@CrossOrigin
+public class EmpleadoServices {
+
+    @Autowired
+    empleadoRepository empleadoRepository;
+
+    @GetMapping(path = "/consulta")
+    private List<Empleado> consulta(){return empleadoRepository.findAll();}
+
+}
