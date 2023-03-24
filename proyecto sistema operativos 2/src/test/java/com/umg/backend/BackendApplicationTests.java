034920e2857b4bd5563c9cef2ae841279b1ea7b8
Cristian Cáceres <60829287+kriskcrs@users.noreply.github.com>
2023-03-23 22:13:29 -0600

    Creacion de proyecto
    Se sube el proyecto

---
+++ proyecto sistema operativos 2/src/test/java/com/umg/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
