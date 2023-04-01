07380fa695e00b2b728d7c48244b6454e9556052
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-03-31 22:53:18 -0600

    cambios en login

--- proyecto sistema operativos 2/src/main/java/com/umg/backend/service/LoginService.java
+++ proyecto sistema operativos 2/src/main/java/com/umg/backend/service/LoginService.java
@@ -6,6 +6,7 @@ import com.umg.backend.repository.usuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.Optional;
 
 @RestController
@@ -17,14 +18,23 @@ public class LoginService {
     usuarioRepository usuarioRepository;
 
     @PostMapping(path = "/login")
-    private Usuario Authentication(@RequestBody Usuario usuario) {
+    private Usuario Authentication(@RequestBody Usuario usuario) throws IllegalAccessException {
 
+        System.out.println(" Usuario recibdo -> " +usuario.getUsuario() + " contraseña recibida -> " + usuario.getPassword() );
         Optional<Usuario> usuariodata = usuarioRepository.findUsuarioByUsuarioAndPassword(usuario.getUsuario(), usuario.getPassword());
+
         if(usuariodata.isEmpty()){
             usuario.setUsuario("");
             usuario.setPassword("");
         }
         else{
+            Usuario usuarioEncontrado = usuariodata.get();
+            for (Field campo : Usuario.class.getDeclaredFields()) {
+                campo.setAccessible(true); // Permite acceder a campos privados
+                Object valorCampo = campo.get(usuarioEncontrado);
+                System.out.println(campo.getName() + ": " + valorCampo);
+                campo.setAccessible(false); // Restablece el acceso a campos privados
+            }
             usuario.setUsuario(usuariodata.get().getUsuario());
             usuario.setIdUsuario(usuariodata.get().getIdUsuario());
             usuario.setPassword(usuariodata.get().getPassword());
